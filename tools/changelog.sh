670757d643e616c405b5853fbb9851492daed4a1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-17 17:30:16 -0300

    vendor: changelog: Cleanup
    Change-Id: I3ecf4d9d59e033f34f4765877afab9a347b3aa83
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -4,8 +4,10 @@ export Changelog=Changelog.txt
 
 if [ -f $Changelog ];
 then
-	rm -f $Changelog
+    rm -f $Changelog
 fi
+
+export formatter_script=$(realpath vendor/aosp/tools/changelog_repo_formatter.sh)
 
 touch $Changelog
 
@@ -13,23 +15,20 @@ echo "Generating changelog..."
 
 for i in $(seq 14);
 do
-export After_Date=`date --date="$i days ago" +%m-%d-%Y`
-k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+    export After_Date=`date --date="$i days ago" +%Y/%m/%d`
+    k=$(expr $i - 1)
+    export Until_Date=`date --date="$k days ago" +%Y/%m/%d`
 
-	# Line with after --- until was too long for a small ListView
-	echo '=======================' >> $Changelog;
-	echo  "     "$Until_Date       >> $Changelog;
-	echo '=======================' >> $Changelog;
-	echo >> $Changelog;
+    # Line with after --- until was too long for a small ListView
+    echo '=======================' >> $Changelog;
+    echo  "     "$Until_Date       >> $Changelog;
+    echo '=======================' >> $Changelog;
+    echo >> $Changelog;
 
-	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
-	echo >> $Changelog;
+    # Cycle through every repo to find commits between 2 dates
+    repo forall -c '. $formatter_script' >> $Changelog
+    echo >> $Changelog;
 done
-
-sed -i 's/project/   */g' $Changelog
 
 cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/
-rm -rf $Changelog
