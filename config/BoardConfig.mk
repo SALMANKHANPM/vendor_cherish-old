49439832f035ade923a60b75a0835f95f23bff67
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:59:36 -0300

    vendor: Force latest redfin build fingerprint
    Change-Id: I57ba4cc2443c9fba0a7233e2d942de87c8ae6a9a
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,5 +1,8 @@
 include vendor/cherish/config/BoardConfigKernel.mk
 
+# Safetynet
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
+
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/cherish/config/BoardConfigQcom.mk
 endif
