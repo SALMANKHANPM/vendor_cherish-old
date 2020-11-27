TheParasiteProject/vendor_parasite_common

vendor: Import overlays from RD1A.201105.003.C1
Change-Id: I1e698ee67e168dd56284e043cabb0544cb5f0b45

config/common.mk
@@ -104,8 +104,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aosp/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aosp/overlay \
+    vendor/aosp/overlay-pixel \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay/common \
+    vendor/aosp/overlay-pixel/common
 
 # TouchGestures
 PRODUCT_PACKAGES += \

SALMANKHANPM/vendor_cherish-old

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -147,8 +147,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/cherish/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/cherish/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/cherish/overlay \
+    vendor/cherish/overlay-pixel \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/cherish/overlay/common \
+    vendor/cherish/overlay-pixel/common
 
 # OmniStyle
 #PRODUCT_PACKAGES += \