Evolution-X-AOSP/vendor_evolution

config: Use flipendo/turbo_adapter sepolicy from pixel common
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -127,6 +127,9 @@ PRODUCT_PACKAGES += \
 # Branding
 include vendor/evolution/config/branding.mk
 
+# Flipendo
+include vendor/evolution/config/system_sepolicy.mk
+
 # OTA
 include vendor/evolution/config/ota.mk
 

SALMANKHANPM/vendor_cherish-old

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -215,6 +215,9 @@ include vendor/cherish/config/branding.mk
 # Include CherishOS Themes Styles
 include vendor/themes/themes.mk
 
+# Flipendo
+include vendor/cherish/config/system_sepolicy.mk
+
 # OTA
 include vendor/cherish/config/ota.mk
 