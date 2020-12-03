PixelExperience/vendor_aosp

extract_utils: Fixup blueprint/makefile header generation
Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
Change-Id: Ie6ef2470c3c91eec47ebd24da10b96e05b050e56

build/tools/extract_utils.sh
@@ -930,17 +930,17 @@ function write_blueprint_header() {
     [ "$COMMON" -eq 1 ] && local DEVICE="$DEVICE_COMMON"
 
     printf "/**\n" > $1
-    if [ $INITIAL_COPYRIGHT_YEAR -lt 2019 ]; then
+    if [[ $INITIAL_COPYRIGHT_YEAR -lt 2019 ]]; then
         BLUEPRINT_INITIAL_COPYRIGHT_YEAR=2019
     else
         BLUEPRINT_INITIAL_COPYRIGHT_YEAR=$INITIAL_COPYRIGHT_YEAR
     fi
 
     NUM_REGEX='^[0-9]+$'
-    if [ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -eq $YEAR ]; then
+    if [[ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -eq $YEAR ]]; then
         printf " * Copyright (C) $YEAR The LineageOS Project\n" >> $1
         printf " * Copyright (C) $YEAR The PixelExperience Project\n" >> $1
-    elif [ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -le 2019 ]; then
+    elif [[ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -le 2019 ]]; then
         printf " * Copyright (C) 2019-$YEAR The LineageOS Project\n" >> $1
         printf " * Copyright (C) 2019-$YEAR The PixelExperience Project\n" >> $1
     else
@@ -1007,8 +1007,8 @@ function write_makefile_header() {
             printf "# Copyright (C) $INITIAL_COPYRIGHT_YEAR-$YEAR The PixelExperience Project\n" >> $1
         fi
     else
-        printf "# Copyright (C) $YEAR The LineageOS Project\n" > $1
-        printf "# Copyright (C) $YEAR The PixelExperience Project\n" > $1
+        printf "# Copyright (C) $YEAR The LineageOS Project\n" >> $1
+        printf "# Copyright (C) $YEAR The PixelExperience Project\n" >> $1
     fi
 
     cat << EOF >> $1

SALMANKHANPM/vendor_cherish-old

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

build/tools/extract_utils.sh
@@ -931,18 +931,18 @@ function write_blueprint_header() {
     [ "$COMMON" -eq 1 ] && local DEVICE="$DEVICE_COMMON"
 
     printf "/**\n" > $1
-    if [ $INITIAL_COPYRIGHT_YEAR -lt 2019 ]; then
+    if [[ $INITIAL_COPYRIGHT_YEAR -lt 2019 ]]; then
         BLUEPRINT_INITIAL_COPYRIGHT_YEAR=2019
     else
         BLUEPRINT_INITIAL_COPYRIGHT_YEAR=$INITIAL_COPYRIGHT_YEAR
     fi
 
     NUM_REGEX='^[0-9]+$'
-    if [ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -eq $YEAR ]; then
+    if [[ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -eq $YEAR ]]; then
         printf " * Copyright (C) $YEAR The CherishOS Project\n" >> $1
         printf " * Copyright (C) $YEAR The LineageOS Project\n" >> $1
         printf " * Copyright (C) $YEAR The PixelExperience Project\n" >> $1
-    elif [ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -le 2019 ]; then
+    elif [[ $BLUEPRINT_INITIAL_COPYRIGHT_YEAR -le 2019 ]]; then
         printf " * Copyright (C) 2019-$YEAR The CherishOS Project\n" >> $1
         printf " * Copyright (C) 2019-$YEAR The LineageOS Project\n" >> $1
         printf " * Copyright (C) 2019-$YEAR The PixelExperience Project\n" >> $1
@@ -1011,8 +1011,8 @@ function write_makefile_header() {
             printf "# Copyright (C) $INITIAL_COPYRIGHT_YEAR-$YEAR The LineageOS Project\n" >> $1
         fi
     else
-        printf "# Copyright (C) $YEAR The CherishOS Project\n" > $1
-        printf "# Copyright (C) $YEAR The LineageOS Project\n" > $1
+        printf "# Copyright (C) $YEAR The CherishOS Project\n" >> $1
+        printf "# Copyright (C) $YEAR The LineageOS Project\n" >> $1
     fi
 
     cat << EOF >> $1